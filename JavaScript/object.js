b84ecab5034590b1bfe6f769c9b62f54d0bebbd7
NarayanNandi <narayannandi357@gmail.com>
2026-02-22 23:14:54 +0530

    obj

---
+++ JavaScript/object.js
@@ -0,0 +1,8 @@
+const Student = {
+    fullName:"Narayan Nandi",
+    roll:12100123099,
+    isPass:true,
+};
+
+console.log("Student Name is: ",Student["fullName"]);
+console.log("Student roll is: ",Student.roll);
